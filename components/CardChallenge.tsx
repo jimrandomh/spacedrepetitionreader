2dba5d2bce2a6f2dbd574e6c0d24492fc6d589bc
James Babcock <jimrandomh@gmail.com>
2022-12-13 17:52:55 -0800

    Basic card-presentation UI

---
+++ components/CardChallenge.tsx
@@ -0,0 +1,38 @@
+import React,{useState} from 'react'
+
+export function CardChallenge({card}: {
+  card: {
+    front: string,
+    back: string
+  }
+}) {
+  const [flipped,setFlipped] = useState(false);
+  
+  function clickFlip() {
+    setFlipped(true);
+  }
+  
+  function clickResolution(resolution: "Easy"|"Hard"|"Repeat") {
+    setFlipped(false); // TODO
+  }
+  
+  if (flipped) {
+    return <div className="cardChallenge">
+      <div className="content">{card.back}</div>
+      
+      <div className="buttons">
+        <div className="button easy" onClick={ev => clickResolution("Easy")}>Easy</div>
+        <div className="button hard" onClick={ev => clickResolution("Hard")}>Hard</div>
+        <div className="button again" onClick={ev => clickResolution("Repeat")}>Repeat</div>
+      </div>
+    </div>
+  } else {
+    return <div className="cardChallenge">
+      <div className="content">{card.front}</div>
+      
+      <div className="buttons">
+        <div className="button flip" onClick={ev => clickFlip()}>Flip</div>
+      </div>
+    </div>
+  }
+}
--- pages/FrontPage.tsx
+++ pages/FrontPage.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import {TopBar} from '../components/TopBar';
 import {LeftSidebar} from '../components/LeftSidebar';
+import {CardChallenge} from '../components/CardChallenge';
 
 export function FrontPage() {
   return <div className="frontPage">
@@ -8,6 +9,10 @@ export function FrontPage() {
     <LeftSidebar/>
     
     <div className="mainPane">
+      <CardChallenge card={{
+        front: "Why do people stop using spaced repetition apps?",
+        back: "Homework assigned by your past self is still homework",
+      }}/>
     </div>
   </div>
 }
