649db9a492c1e27b40340580004ba2c8c49b72ab
James Babcock <jimrandomh@gmail.com>
2022-12-15 16:22:08 -0800

    Fix evaluation of card due-dates

--- server/cardScheduler.ts
+++ server/cardScheduler.ts
@@ -11,16 +11,17 @@ export function getDueDate(card: Card, pastImpressions: CardImpression[], ctx: S
   // TODO PLACEHOLDER: A card is due a fixed duration after its last impression depending on the result
   const sortedImpressions = orderBy(pastImpressions, imp=>imp.date);
   const lastImpression = sortedImpressions[sortedImpressions.length-1];
+  const lastImpressionTime = lastImpression.date.getTime();
   const now = new Date().getTime();
   
   switch (lastImpression.resolution) {
     default:
     case "Easy":
-      return new Date(now + 60*60*1000); //1hr
+      return new Date(lastImpressionTime + 60*60*1000); //1hr
     case "Hard":
-      return new Date(now + 5*60*1000); //5min
+      return new Date(lastImpressionTime + 5*60*1000); //5min
     case "Repeat":
-      return new Date(now + 5*1000); //5s
+      return new Date(lastImpressionTime + 5*1000); //5s
   }
 }
 
