43407863cb24ecf56ac54aed0b429c4277c52d2e
James Babcock <jimrandomh@gmail.com>
2023-08-27 10:30:41 +0000

    Rewrite runProduction.sh for OVH deploy

--- scripts/runProduction.sh
+++ scripts/runProduction.sh
@@ -1,33 +1,12 @@
 #!/bin/bash
 
-## runProduction.sh: Run the server in production. This uses the enviornment variable
-## GITHUB_DEPLOY_KEY as an ssh priate key to load the github repo named in
-## GITHUB_CREDENTIALS_REPO, which should contain a config.js (which in turn contains
-## the database password and other configuration)
-#
-## GITHUB_DEPLOY_KEY should be an SSH private key, which is added to the credentials
-## repo as described in: https://docs.github.com/en/authentication/connecting-to-github-with-ssh/managing-deploy-keys
-#echo "$GITHUB_DEPLOY_KEY" >ssh_key
-#
-#echo "Cloning credentials repo"
-#GIT_SSH_COMMAND="ssh -v -i ssh_key -o StrictHostKeyChecking=no" git clone ssh://git@github.com/$GITHUB_CREDENTIALS_REPO Credentials
-#
-#echo "Copying config"
-#cp Credentials/config.js .
-#
-#echo "Cleaning up temporaries"
-#rm ssh_key
-#rm -rf Credentials
-
-echo "$SERVER_CONFIG" >config.js
+export NVM_DIR="$HOME/.nvm"
+. "$NVM_DIR/nvm.sh"
+nvm use 18
+node --version
 
 echo "Running yarn install"
-yarn --network-timeout 100000 install
-
-echo "Running apt-get install"
-apt-get install build-essential libpq-dev
-
-yarn prisma generate
+yarn install
 
 echo "Starting server"
 ./build.js --watch --run
