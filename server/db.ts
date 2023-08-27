6e50caa25eb6fc038a8c409a2ddffce40ec0ac79
James Babcock <jimrandomh@gmail.com>
2023-08-27 12:49:38 -0700

    Add a comment about how to log DB queries

--- server/db.ts
+++ server/db.ts
@@ -6,7 +6,10 @@ export function getPrisma(): PrismaClient {
   if (prisma)
     return prisma;
   
-  prisma = new PrismaClient()
+  prisma = new PrismaClient({
+    // Uncomment to look every SQL query
+    //log: ["query", "info", "warn", "error"],
+  })
   if (!prisma) throw new Error("Unable to initialize prisma");
   return prisma;
 }
